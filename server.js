01cc7d34efc956b97dfabd43f69f065dcfc8e909
Chinmay <chinmayans08@gmail.com>
2019-02-10 05:51:25 +0530

    Added a logging module to app

--- server.js
+++ server.js
@@ -1,13 +1,53 @@
 const express = require('express');
 const hbs = require('hbs');
 const request =require('request');
-let app = express();
+const expressFingerprint = require('express-fingerprint');
+const fs = require("fs");
+const app = express();
+const logger = require("nodejslogger");
+logger.init({"file":"logger-file","mode":"DIE"});
 const port = process.env.PORT || 3000;
+
+ 
+app.use(expressFingerprint({
+    parameters:[
+        // Defaults
+        expressFingerprint.useragent,
+        expressFingerprint.acceptHeaders,
+        expressFingerprint.geoip,
+ 
+        // Additional parameters
+        // function(next) {
+        //     // ...do something...
+        //     next(null,{
+        //     'param1':'value1'
+        //     })
+        // },
+        // function(next) {
+        //     // ...do something...
+        //     next(null,{
+        //     'param2':'value2'
+        //     })
+        // },
+    ]
+}))
+ 
+// app.get('*',function(req,res,next) {
+//     // Fingerprint object
+//     console.log(req.fingerprint)
+// })
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
+  fs.appendFile("./user-fingerprint-logging.txt",JSON.stringify(req.fingerprint)+"\n",function(err,success){
+    if(err){
+      console.log("Error in storing to file");
+    }
+  });
+  logger.info(JSON.stringify(req.fingerprint)+"\n");
+  console.log(req.fingerprint);
   res.render('home.hbs', {
     pageTitle: "Home page",
     copyRight: "Copy Right 2018 Funny Coders"
